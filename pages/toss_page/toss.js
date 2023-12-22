161b9c9cd6efc89d9b864742b1a22d03e7a9a9de
manishseervi-iisc <seervinirmal1996@gmail.com>
2023-12-22 17:09:06 +0530

    done with the toss page

--- pages/toss_page/toss.js
+++ pages/toss_page/toss.js
@@ -1,40 +1,40 @@
-
-
-const head=document.getElementById("2");
-const tails=document.getElementById("3");
+const head = document.getElementById("head");
+const tails = document.getElementById("tails");
 
 function getResult() {
-    const i=Math.floor(Math.random()*2);
-    
-    if(i==0) return "head";
+    const i = Math.floor(Math.random() * 2);
+
+    if (i == 0) return "head";
     else return "tails";
 }
 
+function toss(choice) {
+    if (choice == (getResult())) { location.replace('../choose_page/index.html') }
 
-
-function toss(choice){
-    if(choice==(getResult())) {location.replace('../choose_page/index.html')}
-        
-    else {document.getElementById("1").textContent="you lose";
-compChoose();}
-head.disabled= true;
-tails.disabled=true;
+    else {
+        document.getElementById("lose").textContent = "Computer Won the Toss";
+        compChoose();
+        document.getElementById("choice").style.display = "none";
+    }
+    head.disabled = true;
+    tails.disabled = true;
 }
 
-function compChoose(){
-    const i=Math.floor(Math.random()*2);
-    if(i==0){
-        document.getElementById("4").textContent="Computer chose to bat";
-        document.getElementById("6").style.display="block";
+function compChoose() {
+    const i = Math.floor(Math.random() * 2);
+    if (i == 0) {
+        document.getElementById("choose").textContent = "Computer chose to bat";
+        document.getElementById("bowl").style.display = "block";
     }
     else {
-        document.getElementById("4").textContent="Computer chose to Bowl";
-    document.getElementById("5").style.display="block";}
+        document.getElementById("choose").textContent = "Computer chose to Bowl";
+        document.getElementById("bat").style.display = "block";
+    }
 }
 
 
 
 
 
-   head.addEventListener("click",()=>toss("head"));
-   tails.addEventListener("click",()=>toss("tails"));
+head.addEventListener("click", () => toss("head"));
+tails.addEventListener("click", () => toss("tails"));
